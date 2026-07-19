d1b0da52e0291d6d0a89ac960109f93d6d6a35cf
hammad24-ox <hamadrafay5@gmail.com>
2026-07-19 10:52:59 +0500

    Add wishlist functionality and refactor cart update

--- script.js
+++ script.js
@@ -1,56 +1,181 @@
 let cart = [];
 
+let wishlist = [];
+
+
 let buttons = document.querySelectorAll(".add-cart");
 
 let cartItems = document.getElementById("cart-items");
+
 let cartCount = document.getElementById("cart-count");
 
 
+
+// Add To Cart
+
 buttons.forEach(button => {
 
-  button.addEventListener("click", () => {
+button.addEventListener("click",()=>{
 
-    let product = button.getAttribute("data-name");
 
-    cart.push(product);
+let product = button.getAttribute("data-name");
 
-    updateCart();
 
-  });
+cart.push(product);
+
+
+updateCart();
+
 
 });
 
 
+});
+
+
+
+
+
+// Update Cart
+
 function updateCart(){
 
-  cartItems.innerHTML = "";
 
-  cart.forEach((item, index) => {
-
-    let li = document.createElement("li");
-
-    li.innerText = item + " ";
-
-    let removeBtn = document.createElement("button");
-
-    removeBtn.innerText = "Remove";
-
-    removeBtn.onclick = function(){
-
-      cart.splice(index, 1);
-
-      updateCart();
-
-    };
+cartItems.innerHTML="";
 
 
-    li.appendChild(removeBtn);
-
-    cartItems.appendChild(li);
-
-  });
+cart.forEach((item,index)=>{
 
 
-  cartCount.innerText = cart.length;
+let li=document.createElement("li");
+
+
+li.innerText=item+" ";
+
+
+
+let remove=document.createElement("button");
+
+
+remove.innerText="Remove";
+
+
+remove.className="btn btn-sm btn-dark";
+
+
+remove.onclick=()=>{
+
+
+cart.splice(index,1);
+
+
+updateCart();
+
+
+};
+
+
+
+li.appendChild(remove);
+
+
+cartItems.appendChild(li);
+
+
+
+});
+
+
+
+cartCount.innerText=cart.length;
+
+
 
 }
+
+
+
+
+
+
+// Wishlist
+
+
+let wishButtons=document.querySelectorAll(".wishlist");
+
+
+
+wishButtons.forEach((button)=>{
+
+
+button.addEventListener("click",()=>{
+
+
+let name=button.parentElement.querySelector("h3").innerText;
+
+
+wishlist.push(name);
+
+
+button.innerText="❤️ Added";
+
+
+alert(name+" added to Wishlist");
+
+
+});
+
+
+});
+
+
+
+
+
+
+
+// Search
+
+
+let search=document.getElementById("search");
+
+
+let products=document.querySelectorAll(".product");
+
+
+
+search.addEventListener("keyup",()=>{
+
+
+let value=search.value.toLowerCase();
+
+
+
+products.forEach(product=>{
+
+
+let text=product.innerText.toLowerCase();
+
+
+
+if(text.includes(value)){
+
+
+product.style.display="block";
+
+
+}
+
+else{
+
+
+product.style.display="none";
+
+
+}
+
+
+});
+
+
+});
