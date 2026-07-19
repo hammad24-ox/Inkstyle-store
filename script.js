a9a3078ce37f215385bee923f8e8abf46898cc5f
hammad24-ox <hamadrafay5@gmail.com>
2026-07-19 09:53:17 +0500

    Add cart functionality to script.js
    Implement cart functionality to add products and update display.

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 console.log("InkStyle Store Loaded Successfully!");
+let cart = [];
+
+let buttons = document.querySelectorAll(".add-cart");
+
+let cartItems = document.getElementById("cart-items");
+let cartCount = document.getElementById("cart-count");
+
+buttons.forEach(button => {
+
+  button.addEventListener("click", () => {
+
+    let product = button.getAttribute("data-name");
+
+    cart.push(product);
+
+    updateCart();
+
+  });
+
+});
+
+
+function updateCart(){
+
+  cartItems.innerHTML = "";
+
+  cart.forEach(item => {
+
+    let li = document.createElement("li");
+
+    li.innerText = item;
+
+    cartItems.appendChild(li);
+
+  });
+
+
+  cartCount.innerText = cart.length;
+
+}
